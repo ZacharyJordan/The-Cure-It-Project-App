b059f91afb3a113f74bdd755b73b381158c43dea
Zachary Jordan <zacharyjordan@Zacharys-MacBook-Pro.local>
2017-01-04 11:53:54 -0600

    Testing inappBrowser

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import {InformationPage} from '../information/information';
+import {InAppBrowser} from 'ionic-native';
 
 @Component({
   selector: 'page-contact',
   templateUrl: 'contact.html'
 })
 export class ContactPage {
+
+
+  facts()
+  {
+    window.open('http://google.com','_self'); 
+  }
 
   informationOpen()
   {
